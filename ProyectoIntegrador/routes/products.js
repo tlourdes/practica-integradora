112c41edecac6360b46407ac5c6b8c115e902613
tlourdes <tlourdes@udesa.edu.ar>
2025-04-07 13:38:57 -0300

    arreglando errror controladores

--- ProyectoIntegrador/routes/products.js
+++ ProyectoIntegrador/routes/products.js
@@ -1,14 +1,14 @@
 // index, product, add product
 const express = require("express")
 const router = express.Router()
-const productosController= require("../controllers/productosControllers")
-const usuarioController= require("../controllers/usuaio")
+const productosController= require("../controllers/productosController")
+const usuarioController= require("../controllers/usuario")
 
 router.get("/", productosController.products)
 
 router.get("/detalle/:id", productosController.product)
 
-router.get("/agregar", productosController.product-add)
+router.get("/agregar", productosController.productAdd)
 
 router.get("/login", usuarioController.login)
 
