a5ce142739456613c8041017e76ac16dd2b19875
amparoricotta <aricottapensa@udesa.edu.ar>
2025-04-10 16:34:07 -0300

    punto 9 e inicios del 10 y 11

--- ProyectoIntegrador/controllers/productosController.js
+++ ProyectoIntegrador/controllers/productosController.js
@@ -1,6 +1,10 @@
+const data = require("../db/product")
 const productosController ={
     products: function(req,res){
-        res.render("products", )
+        res.render("products", {
+
+            productos : data.productos,
+        })
     },
 
     product: function(req,res){
